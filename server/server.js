439cf98ff954a841a21925402f166e08057df5a9
Vinit  Singh <rivshunyrt27@gmail.com>
2024-10-22 14:46:58 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://phishield-mail.vercel.app",
     credentials: true,
   })
 );
